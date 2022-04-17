1f685e2c85b802af6c33e6f4920da0ebd99feea5
DjalNowou <imsour.jalal@gmail.com>
2022-04-17 02:45:33 +0200

    ok

--- src/controllers/articles.php
+++ src/controllers/articles.php
@@ -1,8 +1,7 @@
 <?php
-    $titre = "Articles";
-    app\app::addRessource("style/articles.less");
-    app\app::addRessource("js/articles.js");
-    require("./views/share/header.php");
-    require("./views/articles.php");
-
-?>
+$titre = "Articles";
+app\app::addRessource("style/articles.less");
+app\app::addRessource("js/articles.js");
+require("./views/share/header.php");
+require("./views/articles.php");
+require("./views/share/footer.php");
