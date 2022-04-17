d0b29523c989684e260687a30ad22be22616ab40
DjalNowou <imsour.jalal@gmail.com>
2022-04-17 14:55:13 +0200

    testagain

---
+++ public/views/accueil.php
@@ -0,0 +1,42 @@
+<h1>Accueil</h1>
+
+<div id="Accueil">
+    <div class="container">
+        <div class="row">
+
+            <div class="card" style="width: 50%; height: 25rem;">
+                <img class="card-img-top" src="style/program.png" alt="Card image cap">
+                <!-- <div class="centered">Decouvrez la programation</div> -->
+                <!-- <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div> -->
+            </div>
+
+            <div class="card" style="width: 45%; height: 25rem;">
+                <img class="card-img-top" src="style/intelligenceart.jpg" alt="Card image cap">
+                <!-- <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div> -->
+            </div>
+
+            <div class="card" style="width: 30%; height: 25rem;">
+                <img class="card-img-top" src="style/phpimg.jpeg" alt="Card image cap">
+                <!-- <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div> -->
+            </div>
+
+            <div class="card" style="width: 65%; height: 25rem;">
+                <img class="card-img-top" src="style/metaverse.jpg" alt="Card image cap">
+                <!-- <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div> -->
+            </div>
+
+            <p class="text-information">
+                Tous nos derniers articles sur la programmation et le monde du digital !
+            </p>
+
+        </div>
+    </div>
+</div>
