b6267ad4d289e0dde471d1cfe5fa81a85e287822
DjalNowou <imsour.jalal@gmail.com>
2022-04-18 01:18:07 +0200

    envoi final

--- public/views/articles.php
+++ public/views/articles.php
@@ -8,7 +8,7 @@
             <!-- <h2>La programmation, une nouvelle manière d'appréhender le monde</h2>
             <h3>Depuis des décennies des codeurs refont le monde du bout des doigts</h3>
 
-            <img class="article-img-1" src="style/article-prog.jpg" alt="">
+            <img class="article-img-1" src="images/article-prog.jpg" alt="">
             <figcaption>Différents languages s'offrent a vous dans la programmation</figcaption>
 
             <p>
@@ -24,7 +24,7 @@
             <h4>"Coder pour moi c'est un rêve depuis mon plus jeune âge"</h4>
             <figcaption>Daysi, étudiant au CCI</figcaption>
 
-            <img class="article-img-2" src="style/computer.jpg" alt="">
+            <img class="article-img-2" src="images/computer.jpg" alt="">
             <figcaption>Salle d'ordinateur du CCI</figcaption>
 
             <p>Nous sommes donc parti rencontrer cette jeunesse qui a soif d'avenir et qui veut toucher ce monde digital du bout des doigts. Un récit incontournable</p>
@@ -49,14 +49,19 @@
 
             <p>Daysi : Rien de spécial a part que je trouve Jalal vraiment incroyablement talentueux. </p>
 
-
             <small>Article rédigé par Marion Lecluze - Avril 2022 pour Daily Code </small> -->
 
 
-            <h2>L'intelligence artificielle : le nouvel eldorado</h2>
+
+
+
+
+
+
+            <!-- <h2>L'intelligence artificielle : le nouvel eldorado</h2>
             <h3>De plus en plus de développeurs s'intéresse a cette technologique</h3>
 
-            <img class="article-img-1" src="style/inteliagi.jpg" alt="">
+            <img class="article-img-1" src="images/inteliagi.jpg" alt="">
             <figcaption>Il n'y a plus qu'un pas entre l'humain et le robot</figcaption>
 
             <p>
@@ -72,7 +77,7 @@
             <h4>"D'ici 30 ans, les intelligences artificielles dirigeront le monde"</h4>
             <figcaption>Michael Connor, CEO de IntelligensiaArt</figcaption>
 
-            <img class="article-img-2" src="style/robotameca.png" alt="">
+            <img class="article-img-2" src="images/robotameca.png" alt="">
             <figcaption>Le robot Ameca créé par une startup américaine</figcaption>
 
             <p>Nous avons rencontrés Michael Connor qui dirige un cabinet de conseil étudiant les intelligences artificielles a travers le monde</p>
@@ -88,6 +93,98 @@
             <p class="text-question"> Quel language est le plus aproprié pour apprendre l'IA ? </p>
 
             <p>Michael Connor : Je dirais HTML 5 (rires) .. plus sérieusement Python a plus que démontré son importance dans l'intelligence artificielle. . donc foncez sur cette techno !</p>
+
+
+            <small>Article rédigé par Pascal Cravate - Avril 2022 pour Daily Code </small> -->
+
+
+
+
+
+
+
+
+            <!-- <h2>PHP le meilleur language de l'univers</h2>
+            <h3>Après des années de comparaison nous avons trouvé LE language de programmation</h3>
+
+            <img class="article-img-1" src="images/phpimagearticle.jpg" alt="">
+            <figcaption>Le language existe depuis 1994</figcaption>
+
+            <p>
+                Loin des débats sans fin pour savoir le meilleur language de programmation, aujourd'hui il est clair et net que PHP se détache de tous. Plus de trois quart de l'internet est sous PHP et ce n'est pas pour rien. Travaillant étroitement avec du HTML et de par ses modèles de vues mais aussi sa facilité d'execution, PHP n'est pas qu'un language de "dinosaure" il est aussi celui qui redéfinit toujours les codes et ne prend pas une ride
+            </p>
+
+
+
+            <p>
+                Aujourd'hui, beaucoup de nouveaux développeurs font le pari de se lancer sur de la programmation en commencant par du Python ou du Javascript, néanmoins PHP peut vous donner rapidement des bases solides sur la programmation. Que ce soit en programmation procédurale ou bien en programmation objet, PHP se montre versatile est accessibles pour un plus grand nombre. Il est d'aujourd'hui de la cour des grands et des languages incontournables.
+            </p>
+
+            <h4>"Je donnerais ma vie pour ce language"</h4>
+            <figcaption>Roger Cauchon, maître sensei</figcaption>
+
+            <img class="article-img-2" src="images/phpimagearticle2.jpeg" alt="">
+            <figcaption>PHP reste a ce jour un incontournable</figcaption>
+
+            <p>Roger Cauchon, expert incontournable de PHP nous raconte pourquoi c'est pour lui le meilleur language</p>
+
+            <p class="text-question">Pourquoi selon vous c'est le language incontournable ?</p>
+
+            <p>Roger Cauchon : C'est simple, pour moi il n'y a rien de mieux que les spacename ou les "model view controller" je ne jure que par ça. Non seulement je dors PHP mais je rêve PHP. Quand je me rase je pense uniquement au PHP. C'est une façon de vivre pour moi</p>
+
+            <p class="text-question"> Pensez vous qu'il y aura une fin du PHP ? </p>
+
+            <p>Roger Cauchon : Vous savez j'ai été le premier a me battre sur du PHP en 1994, le premier français a faire un site sous cette technologie, il n'y aura pas de fin mais s'il y en a une je serais le dernier humain sur terre a l'utiliser ! </p>
+
+            <p class="text-question"> Avez-vous connu des experts dans le domaine ? </p>
+
+            <p>Roger Cauchon : Sans aucun doute Jalal Imsour et Daysi Vakhaev, les deux m'ont subjugué de par leur maîtrise du language et la compréhension. Des génies. Je crois même qu'ils ont créé Facebook a la place de Zuckerberg en même pas 15 minutes. Des vraies machines ! </p>
+
+
+            <small>Article rédigé par Marion Lecluze - Avril 2022 pour Daily Code </small> -->
+
+
+
+
+
+
+
+
+            <h2>Les metaverses : le futur d'aujourd'hui</h2>
+            <h3>Comment les réseaux sociaux vont vite être remplacés par les métaverses</h3>
+
+            <img class="article-img-1" src="images/metaversesarticle.png" alt="">
+            <figcaption>Un monde digital en 3D</figcaption>
+
+            <p>
+                Aujourd'hui l'internet 2.0 que nous connaissons tous a fait bien plus que ses preuves. Les réseaux sociaux font parti inhérente de nos vies et constitue un socle commun de la plupart des sociétés occidentales. Avec l'arrivée des cryptomonnaies mais aussi plus récemment des NFT (Non Fungible Token), internet prend un virage impressionant vers la digitalisation complète. Les métaverses ces univers en 3D en sont la preuve..
+            </p>
+
+
+
+            <p>
+                Facebook vous connaissez comme 3 milliards d'utilisateurs sans aucun doute. Et bien sachez que la firme de Zuckerberg s'appelle dorénavant Meta. Bien évidemment le diminutif de "métaverse". Car la société au pouce bleu a vite compris que tout se jouer dans ces nouveaux mondes virtuels où vous avez la possibilité de visiter un monde juste à l'aide d'un casque virtuel.
+            </p>
+
+            <h4>"Toutes les grande sociétés investissent déjà dans ces mondes"</h4>
+            <figcaption>Phil Johnson, CEO d'une startup côté en bourse</figcaption>
+
+            <img class="article-img-2" src="images/metaversesarticle2.jpg" alt="">
+            <figcaption>Vous pouvez dorénavant faire des confcall en 3D</figcaption>
+
+            <p>Phil Johnson, par le biais de sa société a déjà investi dans ces nouveaux metaverses</p>
+
+            <p class="text-question">Comment est-ce que l'avenir se dessine selon vous ?</p>
+
+            <p>Phil Johnson : C'est très simple, vous avez dorénavant de gros Metaverses comme Meta, Sandbox qui propose des parcelles de terrain a des montant astronomiques. Avoir une place dans ces univers contribue a devenir une carte postale digitale. Les sites seront toujours présents mais une maison dans les métaverses constituera l'avenir de demain</p>
+
+            <p class="text-question"> Comment expliquez vous cet entrain ? </p>
+
+            <p>Phil Johnson : L'arrivée du Bitcoin il y a de cela 10 ans a grandement contribué a tout ce buzz médiatique. Mais je pense que c'est réellement les NFT avec la vente d'oeuvres d'arts digitalisés qui a fait comprendre a un grand nombre de personne que l'on pouvait digitaliser de nombreux items </p>
+
+            <p class="text-question"> Conseillez-vous aux jeunes d'investir dedans ? </p>
+
+            <p> Phil Johnson : Je pense qu'il faut néanmoins être prudent et ne pas sombrer dans la spéculation. Bien évidemment que les enjeux financiers s'y joueront prochainement, néanmoins il faut toujours y aller avec parcimonie. </p>
 
 
             <small>Article rédigé par Pascal Cravate - Avril 2022 pour Daily Code </small>
