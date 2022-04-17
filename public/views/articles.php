3c0270bb0b4ef2e024bce0ee1c6af50a18737013
DjalNowou <imsour.jalal@gmail.com>
2022-04-17 14:32:46 +0200

    commit

--- public/views/articles.php
+++ public/views/articles.php
@@ -1,37 +1,57 @@
 <h1>Articles</h1>
 
-<div id="accueil">
+<div class="articles">
     <div class="container">
         <div class="row">
 
-            <div class="card" style="width: 50%; height: 25rem;">
-                <img class="card-img-top" src="style/program.png" alt="Card image cap">
-                <div class="centered">Decouvrez la programation</div>
-                <!-- <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div> -->
-            </div>
 
-            <div class="card" style="width: 45%; height: 25rem;">
-                <img class="card-img-top" src="" alt="Card image cap">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
+            <h2>La programmation, une nouvelle manière d'appréhender le monde</h2>
+            <h3>Depuis des décennies des codeurs refont le monde du bout des doigts</h3>
 
-            <div class="card" style="width: 20%; height: 25rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
+            <img class="article-img-1" src="style/article-prog.jpg" alt="">
+            <figcaption>Différents languages s'offrent a vous dans la programmation</figcaption>
 
-            <div class="card" style="width: 75%; height: 25rem;">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
+            <p>
+                Et si la programmation finalement n'était pas la révolution de ce nouveau millénaire Tellement d'outil de la vie de tous les jours font aujourd'hui appel a du code, que ce soit votre machine à café, votre téléphone, ou même votre voiture, toutes vos habitudes sont gérés par la programmation. Incroyable non ?
+            </p>
+
+
+
+            <p>
+                De nombreux scientifiques et économistes s'accorder a dire que le demain sera d'ingénieur informatique principalement. C'est pourquoi en France plus de 80 000 postes sont à pourvoir sur les 10 prochaines années, un nombre certes impressionant mais qui répond a un marché du travail en total expansion. De plus en plus de jeunes ou moins jeunes souhaitent se former au moitié de développeurs web, tout comme c'est le cas au CCI de DREUX avec Jalal Imsour et Daysi Vakhaev.
+            </p>
+
+            <h4>"Coder pour moi c'est un rêve depuis mon plus jeune âge"</h4>
+            <figcaption>Daysi, étudiant au CCI</figcaption>
+
+            <img class="article-img-2" src="style/computer.jpg" alt="">
+            <figcaption>Salle d'ordinateur du CCI</figcaption>
+
+            <p>Nous sommes donc parti rencontrer cette jeunesse qui a soif d'avenir et qui veut toucher ce monde digital du bout des doigts. Un récit incontournable</p>
+
+            <p class="text-question">Pourquoi cette envie d'aller vers la programmation ?</p>
+
+            <p>Jalal : J'ai toujours eu une passion grandissante vers le monde de l'informatique et c'était pour moi l'occasion de concrétiser cela</p>
+
+            <p>Daysi : Moi depuis tout jeune je faisais des mini fusées avec du C++ un language de programmatin assez basique, c'est inné</p>
+
+            <p class="text-question">Comment vous sentez vous en formation ? </p>
+
+            <p>Daysi : Très bien, j'aime beaucoup l'ambiance et les parties de foot pendant la pause</p>
+
+            <p>Jalal : Je suis quelqu'un de très sérieux et rigoureux j'ai pas le temps de m'amuser dehors. Je préfère jouer a des jeux vidéos sur mon pc pendant le cours</p>
+
+            <p class="text-question">Y'a-t-il une manière que vous préferez ? </p>
+
+            <p>Daysi et Jalal : Sans aucun doute le cours de PHP avec Monsieur Cauchon, c'est vraiment le meilleur prof du monde !</p>
+
+            <p class="text-question">Un autre mot ? </p>
+
+            <p>Daysi : Rien de spécial a part que je trouve Jalal vraiment incroyablement talentueux. </p>
+
+
+            <small>Article rédigé par Jacques La Virandière - Avril 2022 pour Daily Code </small>
+
 
 
         </div>
