a84272931f938c58ed06797b33fafaf2885f02ae
DjalNowou <imsour.jalal@gmail.com>
2022-04-17 19:25:44 +0200

    okkkkk

--- public/views/articles.php
+++ public/views/articles.php
@@ -5,7 +5,7 @@
         <div class="row">
 
 
-            <h2>La programmation, une nouvelle manière d'appréhender le monde</h2>
+            <!-- <h2>La programmation, une nouvelle manière d'appréhender le monde</h2>
             <h3>Depuis des décennies des codeurs refont le monde du bout des doigts</h3>
 
             <img class="article-img-1" src="style/article-prog.jpg" alt="">
@@ -50,7 +50,47 @@
             <p>Daysi : Rien de spécial a part que je trouve Jalal vraiment incroyablement talentueux. </p>
 
 
-            <small>Article rédigé par Marion Lecluze - Avril 2022 pour Daily Code </small>
+            <small>Article rédigé par Marion Lecluze - Avril 2022 pour Daily Code </small> -->
+
+
+            <h2>L'intelligence artificielle : le nouvel eldorado</h2>
+            <h3>De plus en plus de développeurs s'intéresse a cette technologique</h3>
+
+            <img class="article-img-1" src="style/inteliagi.jpg" alt="">
+            <figcaption>Il n'y a plus qu'un pas entre l'humain et le robot</figcaption>
+
+            <p>
+                L'intelligence artificielle définit de plus en plus nos vies, que ce soit avec Siri, Alexa ou Cortana, nous faisons de plus en plus appel à ces intelligences pour nous aider et nous guider dans nos vies de tous les jours. Aussi incroyable que cela puisse paraître, nous en sommes encore qu'au balbutiement, la technologique a encore de beaux jours devant elle
+            </p>
+
+
+
+            <p>
+                Beaucoup de developpeurs privilégient Python a d'autres languages, en effet ce language qui a déjà maintenant plus de 20 ans, c'est fait connaître de par sa caractéristique et son approche très basé sur l'IA. Aujourd'hui les algorithmes (qui sont des formes d'intelligences artificielles moins poussés) définissent nos vies et notre consommation d'internet, Youtube, Netflix, Twitter, tout est question d'algortihme. Même d'un point de vue national ou international, des structures de plus en plus grandes font appel aux algorithmes.
+            </p>
+
+            <h4>"D'ici 30 ans, les intelligences artificielles dirigeront le monde"</h4>
+            <figcaption>Michael Connor, CEO de IntelligensiaArt</figcaption>
+
+            <img class="article-img-2" src="style/robotameca.png" alt="">
+            <figcaption>Le robot Ameca créé par une startup américaine</figcaption>
+
+            <p>Nous avons rencontrés Michael Connor qui dirige un cabinet de conseil étudiant les intelligences artificielles a travers le monde</p>
+
+            <p class="text-question">Doit-on avoir peur des robots ?</p>
+
+            <p>Michael Connor : Je pense pas que "peur" soit le bon mot. Car si nous avons peur des robots dans ces cas là nous avons peur de nous mêmes. Il faut faire confiance en notre savoir faire. Des ingénieurs du monde entier travaille là dessus depuis des années et je pense que la robotique n'a pas a rougir de son savoir faire</p>
+
+            <p class="text-question"> Est-ce qu'une intelligence artificielle peut être plus intelligente qu'un humain ? </p>
+
+            <p>Michael Connor : Je pense que oui. En effet plus l'IA accumulera des informations et plus elle sera performante. Le Machine Learning c'est la base de l'IA. Et avec cette technologique on peut aller très très loin. </p>
+
+            <p class="text-question"> Quel language est le plus aproprié pour apprendre l'IA ? </p>
+
+            <p>Michael Connor : Je dirais HTML 5 (rires) .. plus sérieusement Python a plus que démontré son importance dans l'intelligence artificielle. . donc foncez sur cette techno !</p>
+
+
+            <small>Article rédigé par Pascal Cravate - Avril 2022 pour Daily Code </small>
 
 
 
